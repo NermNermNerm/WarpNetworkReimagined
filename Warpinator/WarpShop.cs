14b7782e07672f9edc1727f15720fc879c680ab0
Steve Benz <s8878992@hotmail.com>
2026-02-24 20:05:58 -0800

    Troll now warps in and out

--- Warpinator/WarpShop.cs
+++ Warpinator/WarpShop.cs
@@ -6,12 +6,157 @@ namespace NermNermNerm.Warpinator;
 
 public class WarpShop  : ModLet
 {
+    private static readonly Vector2 boothTile = new Vector2(84, 20);
     public override void Entry(ModEntry mod)
     {
         base.Entry(mod);
 
         mod.Helper.Events.GameLoop.DayStarted += this.GameLoopOnDayStarted;
 
+        mod.Helper.Events.GameLoop.UpdateTicked += this.OnUpdateTicked;
+    }
+
+    private int proximityCounter = 0;
+    // private int facingCounter = 0;
+    private int idleCounter = 0;
+
+    // ISSUE:  Could be that a mod would want to change this - so it'd be better if it was loaded from content somewhere.
+    private const int BoothLocationX = 84;
+    private const int BoothLocationY = 20;
+
+    private const int BoothWidthInTiles = 4;
+    private const int BoothHeightInTiles = 5;
+
+    private const string BoothSheetId = "NermNermNerm.Warpinator.TrollBooth";
+
+
+    private enum BoothAnimationFrame
+    {
+        Empty = 0,
+        Poof1,
+        Poof2,
+        Poof3,
+        Poof4,
+        Poof5,
+        Present,
+        Blink,
+        EyesLeft,
+        FaceRight,
+    }
+
+    private const int NumAnimationFrames = 1 + (int)BoothAnimationFrame.FaceRight;
+
+    private record BoothAnimation(BoothAnimationFrame AnimationFrame, int NumTicks);
+
+    private readonly List<BoothAnimation> boothAnimations = new();
+
+    private StaticTile[]? tiles = null;
+
+    private BoothAnimationFrame nowShowingFrame = BoothAnimationFrame.Empty;
+
+    private bool IsNorvinPresent => this.nowShowingFrame >= BoothAnimationFrame.Present;
+
+    private void SetBoothFrame(BoothAnimationFrame newFrame)
+    {
+        var mountain = Game1.getLocationFromName("Mountain");
+        var buildingsLayer = mountain.Map.GetLayer(I("Buildings"));
+        var frontLayer = mountain.Map.GetLayer(I("Front"));
+        var sheet = mountain.Map.GetTileSheet(WarpShop.BoothSheetId);
+
+        for (int dy = 0; dy < WarpShop.BoothHeightInTiles; ++dy) // The png is 80px tall
+        {
+            for (int dx = 0; dx < WarpShop.BoothWidthInTiles; ++dx) // and 64px wide.
+            {
+                int x = WarpShop.BoothLocationX + dx;
+                int y = WarpShop.BoothLocationY + dy;
+                int tileIndex = (int)newFrame * WarpShop.BoothWidthInTiles + dx
+                        + dy * (WarpShop.NumAnimationFrames * WarpShop.BoothWidthInTiles); // dy*tiles-per-row
+
+                (dy < 3 ? frontLayer : buildingsLayer).Tiles[x, y] =
+                    new StaticTile(buildingsLayer, sheet, BlendMode.Alpha, tileIndex);
+            }
+        }
+
+        this.nowShowingFrame = newFrame;
+    }
+
+    void OnUpdateTicked(object? sender, UpdateTickedEventArgs e)
+    {
+        if (!Game1.hasLoadedGame)
+        {
+            return;
+        }
+
+        var mountain = Game1.getLocationFromName("Mountain");
+
+        // 1) Proximity check (once per second)
+        if (++this.proximityCounter >= 60)
+        {
+            this.proximityCounter = 0;
+
+            var nearestFarmer = Utility.isThereAFarmerWithinDistance(boothTile, 7, mountain);
+
+            if (nearestFarmer is not null && !this.IsNorvinPresent && this.boothAnimations.Count == 0)
+            {
+                this.boothAnimations.AddRange([
+                    new BoothAnimation(BoothAnimationFrame.Poof1, 5),
+                    new BoothAnimation(BoothAnimationFrame.Poof2, 5),
+                    new BoothAnimation(BoothAnimationFrame.Poof3, 5),
+                    new BoothAnimation(BoothAnimationFrame.Poof4, 5),
+                    new BoothAnimation(BoothAnimationFrame.Poof5, 5),
+                    new BoothAnimation(BoothAnimationFrame.Present, 1),
+                    ]);
+                mountain.playSound("wand");
+            }
+            else if (nearestFarmer is null && this.IsNorvinPresent && this.boothAnimations.Count == 0)
+            {
+                this.boothAnimations.AddRange([
+                    new BoothAnimation(BoothAnimationFrame.Poof5, 5),
+                    new BoothAnimation(BoothAnimationFrame.Poof4, 5),
+                    new BoothAnimation(BoothAnimationFrame.Poof3, 5),
+                    new BoothAnimation(BoothAnimationFrame.Poof2, 5),
+                    new BoothAnimation(BoothAnimationFrame.Poof1, 5),
+                    new BoothAnimation(BoothAnimationFrame.Empty, 1),
+                ]);
+                mountain.playSound("wand");
+            }
+        }
+
+
+        // 2) Facing the nearest player (every 10 ticks)
+        // if (this.norvinPresent && ++this.facingCounter >= 10)
+        //{
+        //    this.facingCounter = 0;
+            // FaceNearestPlayer();
+        //}
+
+        // 3) Idle animations (randomized)
+        // if (this.norvinPresent && ++this.idleCounter >= Game1.random.Next(30, 120))
+        // {
+        //     this.idleCounter = 0;
+        //     // PlayIdleAnimation();
+        // }
+
+        // Roll any animations
+        if (this.boothAnimations.Any())
+        {
+            ++this.idleCounter;
+            var currentAnimation = this.boothAnimations.First();
+            if (this.nowShowingFrame != currentAnimation.AnimationFrame)
+            {
+                this.SetBoothFrame(currentAnimation.AnimationFrame);
+            }
+            else if (this.idleCounter >= currentAnimation.NumTicks)
+            {
+                this.boothAnimations.RemoveAt(0);
+                var newFirst = this.boothAnimations.FirstOrDefault();
+                if (newFirst is not null)
+                {
+                    this.SetBoothFrame(newFirst.AnimationFrame);
+                    this.idleCounter = 0;
+                }
+            }
+        }
     }
 
     private void GameLoopOnDayStarted(object? sender, DayStartedEventArgs e)
@@ -21,33 +166,45 @@ public class WarpShop  : ModLet
         var frontLayer = mountain.Map.GetLayer(I("Front"));
         var backLayer = mountain.Map.GetLayer(I("Back"));
 
-// Load your texture
-        Texture2D tex = this.Mod.Helper.ModContent.Load<Texture2D>("assets/tollbooth.png");
+        if (this.tiles is null)
+        {
+            Texture2D texture = this.Mod.Helper.ModContent.Load<Texture2D>("assets/tollbooth.png");
 
-// Create a tilesheet
-        TileSheet sheet = new TileSheet(
-            id: "NorvinSheet",
-            map: mountain.Map,
-            imageSource: this.Mod.Helper.ModContent.GetInternalAssetName("assets/tollbooth.png").BaseName,
-            sheetSize: new xTile.Dimensions.Size(tex.Width / 16, tex.Height / 16),
-            tileSize: new xTile.Dimensions.Size(16, 16)
-        );
+            // Create a tilesheet
+            TileSheet sheet = new TileSheet(
+                id: WarpShop.BoothSheetId,
+                map: mountain.Map,
+                imageSource: this.Mod.Helper.ModContent.GetInternalAssetName("assets/tollbooth.png").BaseName,
+                sheetSize: new xTile.Dimensions.Size(texture.Width / 16, texture.Height / 16),
+                tileSize: new xTile.Dimensions.Size(16, 16)
+            );
 
-// Add it to the map
-        mountain.Map.AddTileSheet(sheet);
+            mountain.Map.AddTileSheet(sheet);
 
-// VERY IMPORTANT: refresh the map’s internal tilesheet references
-        mountain.Map.LoadTileSheets(Game1.mapDisplayDevice);
+            // VERY IMPORTANT: refresh the map’s internal tilesheet references
+            mountain.Map.LoadTileSheets(Game1.mapDisplayDevice);
 
-        int x = 84;
-        int y = 20;
-        int tileIndex = 0;
+            int numTiles = (texture.Height / 16) * (texture.Width / 16);
+            if (numTiles != WarpShop.BoothWidthInTiles * WarpShop.BoothHeightInTiles * WarpShop.NumAnimationFrames)
+            {
+                this.LogError($"Mismatch between number of tiles in the shop tilesheet and the number declared texture.Width/16={texture.Width/16} texture.height/16={texture.Height/16}");
+            }
+            this.tiles = new StaticTile[numTiles];
+            for (int i = 0; i < texture.Height / 16 * texture.Width / 16; ++i)
+            {
+                this.tiles[i] = new StaticTile(buildingsLayer, sheet, BlendMode.Alpha, i++);
+            }
+        }
+
         for (int dy = 0; dy < 80 / 16; ++dy) // The png is 80px tall
         {
             for (int dx = 0; dx < 64 / 16; ++dx) // and 64px wide.
             {
+                int x = WarpShop.BoothLocationX + dx;
+                int y = WarpShop.BoothLocationY + dy;
+
                 // Clear any existing stuff.
-                Vector2 xy = new Vector2(x + dx, y + dy);
+                Vector2 xy = new Vector2(x,y);
                 // Remove terrain features (trees, bushes, grass, hoe dirt, etc.)
                 mountain.terrainFeatures.Remove(xy);
 
@@ -59,12 +216,12 @@ public class WarpShop  : ModLet
                 {
                     var ltf = mountain.largeTerrainFeatures[i];
 
-                    if (ltf.getBoundingBox().Intersects(new Rectangle((x + dx) * 64, (y + dy) * 64, 64, 64)))
+                    if (ltf.getBoundingBox().Intersects(new Rectangle((x) * 64, (y) * 64, 64, 64)))
                         mountain.largeTerrainFeatures.RemoveAt(i);
                 }
 
                 // Prevent future tree growth.
-                var backTile = backLayer.Tiles[x + dx, y + dy];
+                var backTile = backLayer.Tiles[x, y];
                 if (backTile is null)
                 {
                     var backTileSheet = mountain.Map.GetTileSheet("spring_outdoorsTileSheet") ?? mountain.Map.TileSheets.First();
@@ -72,17 +229,10 @@ public class WarpShop  : ModLet
                     backLayer.Tiles[x, y] = backTile;
                 }
                 backTile.Properties[I("Diggable")] = new xTile.ObjectModel.PropertyValue(I("F"));
-                // backTile.Properties[I("Passable")] = new xTile.ObjectModel.PropertyValue(dy <= 2 ? I("T") : I("F"));
-
-                // if (dy < 3)
-                // {
-                //     sheet.TileIndexProperties[tileIndex][I("Passable")] = new PropertyValue(I("T"));
-                // } // else it defaults to impassable
-
-                (dy < 3 ? frontLayer : buildingsLayer).Tiles[x+dx, y+dy] = new StaticTile(buildingsLayer, sheet, BlendMode.Alpha, tileIndex++);
-                // buildingsLayer.Tiles[x+dx, y+dy].Properties[I("Passable")] = new xTile.ObjectModel.PropertyValue(dy <= 2 ? I("T") : I("F"));
             }
         }
+
+        this.SetBoothFrame(BoothAnimationFrame.Empty);
 
         /* Something from warpnetwork:
 
